ad86e322020413077037812a1ab7736d470c2a8b
geeksusma <jesusmvillarvazquez@gmail.com>
2020-09-11 11:33:13 +0200

    first commit

---
+++ src/main/java/es/geeksusma/mockitoexamples/MockitoexamplesApplication.java
@@ -0,0 +1,13 @@
+package es.geeksusma.mockitoexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoexamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/geeksusma/mockitoexamples/MockitoexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.geeksusma.mockitoexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoexamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
